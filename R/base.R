442674fe8c67dcd4d4f0a8ee4945df867aae4da5
Roland Hollós <hollorol@meteor22.elte.hu>
2018-09-10 15:19:58 +0200

    Updating gitignore, and create the base R file

--- R/base.R
+++ R/base.R
@@ -3,8 +3,7 @@
 #' This function runs the BBGC-MuSo model and reads in its outputfile in a very structured way, and after that plot the results automaticly 
 #' 
 #' @author Roland Hollos
-#' @
-#' @return It depends on the export parameter. The function returns with a matrix with the modell output, or writes this in a file, which is given previously
+#' @return It depends on the export parameter. 
 #' @usage AgroTest()
 #' @import RBBGCMuso
 #' @export
